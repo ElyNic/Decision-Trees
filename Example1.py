4bd94b426f3fb718d406e0d69550542773c76ed2
ElyNic <eleonora.nicolosi583@gmail.com>
2019-10-29 08:25:29 +0000

    Create Example1.py

---
+++ Example1.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+#get the database
+!wget -O drug200.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/drug200.csv
