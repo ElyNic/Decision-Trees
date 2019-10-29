618cf2cd0a5e8efe3fc78aacb42e7f932fabbf6a
ElyNic <eleonora.nicolosi583@gmail.com>
2019-10-29 09:58:39 +0000

    Update Ezample 1
    I wrote all the code

--- Example1.py
+++ Example1.py
@@ -4,3 +4,71 @@ from sklearn.tree import DecisionTreeClassifier
 
 #get the database
 !wget -O drug200.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/drug200.csv
+  
+  #convert SEX and BP to numerical values
+from sklearn import preprocessing
+le_sex = preprocessing.LabelEncoder()
+le_sex.fit(['F','M'])
+X[:,1] = le_sex.transform(X[:,1]) 
+
+le_BP = preprocessing.LabelEncoder()
+le_BP.fit([ 'LOW', 'NORMAL', 'HIGH'])
+X[:,2] = le_BP.transform(X[:,2])
+
+le_Chol = preprocessing.LabelEncoder()
+le_Chol.fit([ 'NORMAL', 'HIGH'])
+X[:,3] = le_Chol.transform(X[:,3]) 
+
+X[0:5]
+
+
+#setting the decision tree
+from sklearn.model_selection import train_test_split
+#The train_test_split will need the parameters:
+#X, y, test_size=0.3, and random_state=3.
+X_trainset, X_testset, y_trainset, y_testset = train_test_split(X, y, test_size=0.3, random_state=3)
+
+#Modeling
+
+
+#We will first create an instance of the DecisionTreeClassifier called drugTree
+#Inside of the classifier, specify criterion="entropy" so we can see the information gain of each node
+drugTree = DecisionTreeClassifier(criterion="entropy", max_depth = 4)
+drugTree # it shows the default parameters
+
+#fit the data with the training feature matrix X_trainset and training response vector y_trainset
+drugTree.fit(X_trainset,y_trainset)
+
+#Prediction
+predTree = drugTree.predict(X_testset)
+print (predTree [0:5])
+print (y_testset [0:5])
+
+#Evaluation
+#Check the accuracy of our model
+
+from sklearn import metrics
+import matplotlib.pyplot as plt
+print("DecisionTrees's Accuracy: ", metrics.accuracy_score(y_testset, predTree))
+
+#Visualization
+# Notice: You might need to uncomment and install the pydotplus and graphviz libraries if you have not installed these before
+# !conda install -c conda-forge pydotplus -y
+# !conda install -c conda-forge python-graphviz -y
+
+from sklearn.externals.six import StringIO
+import pydotplus
+import matplotlib.image as mpimg
+from sklearn import tree
+%matplotlib inline 
+
+dot_data = StringIO()
+filename = "drugtree.png"
+featureNames = my_data.columns[0:5]
+targetNames = my_data["Drug"].unique().tolist()
+out=tree.export_graphviz(drugTree,feature_names=featureNames, out_file=dot_data, class_names= np.unique(y_trainset), filled=True,  special_characters=True,rotate=False)  
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+graph.write_png(filename)
+img = mpimg.imread(filename)
+plt.figure(figsize=(100, 200))
+plt.imshow(img,interpolation='nearest')
